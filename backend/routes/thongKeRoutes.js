a01692034cca9247f6c668dc9ffca20f138e07b9
shadowhira <thanhoc890@gmail.com>
2025-04-25 03:36:36 +0700

    remove old backend

--- backend/routes/thongKeRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express")
-const router = express.Router()
-const thongKeController = require("../controllers/thongKeController")
-
-// Routes cho thống kê
-router.get("/khach-hang", thongKeController.getThongKeKhachHang)
-
-module.exports = router
-
